e8c3037f06e80817679de87984434f811cb06f0c
Udo Schöfer <developer@udos86.de>
2023-12-18 10:24:19 +0100

    updates packages

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,23 +4,17 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '$lib/prisma';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
-import type { Session, User } from '@auth/core/types';
-import type { Adapter } from '@auth/core/adapters';
 
 export const handle = sequence(
 	SvelteKitAuth({
 		trustHost: true,
-		// Typings error workaround
-		// see https://github.com/nextauthjs/next-auth/issues/6106
-		adapter: PrismaAdapter(prisma) as Adapter,
+		adapter: PrismaAdapter(prisma),
 		session: {
 			strategy: 'database',
 			// see https://github.com/nextauthjs/next-auth/issues/6076
 			generateSessionToken: () => crypto.randomUUID()
 		},
 		providers: [
-			// @ts-ignore
-			// see https://github.com/nextauthjs/next-auth/issues/6174
 			GitHub({
 				clientId: GITHUB_CLIENT_ID,
 				clientSecret: GITHUB_CLIENT_SECRET,
@@ -36,11 +30,8 @@ export const handle = sequence(
 		],
 		callbacks: {
 			session(params) {
-				// Typings error workaround
-				// see https://github.com/nextauthjs/next-auth/issues/7132
-				const session: Session = params.session;
-				const user: User = params.user;
-
+				const { session, user } = params;
+				
 				if (session.user !== undefined) {
 					session.user.permissions = user.permissions;
 				}
